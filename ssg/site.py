205f14d590529fea1dabc8175adced526b91000a
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 07:51:01 +0800

    test

---
+++ ssg/site.py
@@ -0,0 +1,27 @@
+"""
+
+"""
+
+from pathlib import Path
+
+
+class Site():
+    def __init__(self,source,dest) -> None:
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+
+    def create_dir(self,path):
+        """ """
+        directory = self.dest / Path.relative_to(self.source)
+        Path.mkdir(directory,parents=True,exist_ok=True)
+
+    def build(self):
+        """ """
+        Path.mkdir(self.dest,parents=True,exist_ok=True)
+
+        for dir in self.source.rglob("*"):
+            if Path.is_dir(dir):
+                self.create_dir(dir)
+
+
