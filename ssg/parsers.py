473ea8086f29244acf33cf9615e2c594fd0e20d9
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 09:03:29 +0800

    a

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -37,5 +37,5 @@ class Parser():
 
     def copy(self,path,source,dest):
         """ """     
-        shutil.copy2(path, dest / path.reative_to(source))     
+        shutil.copy2(path, dest / path.relative_to(source))     
 
