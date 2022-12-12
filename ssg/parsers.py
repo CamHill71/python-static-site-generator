367eabaaf560a8e05186f16c62c80244ff95f3ac
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 08:29:46 +0800

    d

---
+++ ssg/parsers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 08:25:40 2022
+
+@author: Cameron Hill
+"""
+
+
+from typing import List
+from pathlib import Path
+
+
+class Parser():
+    """ """
+    
