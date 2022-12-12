12d6e6295e636885c2d4085317e01f2e83bfcef9
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 08:50:52 +0800

    a

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -26,3 +26,8 @@ class Parser():
         """ """       
         with open(path,'r') as file:
             return file.read()
+
+    def write(self,path,dest,content,ext=".html"):
+        """ """  
+        full_path = self.dest / path.with_suffix(ext).name
+
