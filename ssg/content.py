16e4aca6d9079cead8de84987e72115e9c3f73d3
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 10:00:05 +0800

    a

--- ssg/content.py
+++ ssg/content.py
@@ -15,8 +15,8 @@ from collections.abc import Mapping
 
 class Content(Mapping):
     """"""
-    __delimeter = "^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter,re.MULTILINE)
+    __delimiter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter,re.MULTILINE)
 
     @classmethod
     def load(self,cls,string):
