7fa6e8d2fd08905ea621a915998b1e86ab066acc
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 09:59:15 +0800

    a

---
+++ ssg/content.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 08:25:40 2022
+
+@author: Cameron Hill
+"""
+
+
+import re
+
+from yaml import load,FullLoader
+
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    """"""
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter,re.MULTILINE)
+
+    @classmethod
+    def load(self,cls,string):
+        """ """
+        _,fm,content =self.__regex.split(string,2)
+
+        load(fm,FullLoader)
+
+        return cls(metadata,content)
