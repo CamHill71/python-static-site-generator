d1c1ade8dadf73edadfbaac27af2d6a31662ffb5
Cameron Hill <87802054+NMTChill@users.noreply.github.com>
2022-12-12 08:05:37 +0800

    c

---
+++ ssg.py
@@ -0,0 +1,15 @@
+import typer
+from ssg.site import Site
+
+
+
+
+def main(self,source="content",dest="dist"):
+    """ """
+    config = {source: "source",dest:"dest"}
+    Site(**config).build()
+    
+
+
+
+typer.run(main)
